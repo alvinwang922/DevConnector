0c4691aa82823a91ca591eec2fa4da9752a22a68
Alvin Wang <alvinwang922@gmail.com>
2020-05-28 15:55:10 -0700

    Implemented editing profile functionalities

---
+++ client/src/components/profile-forms/AddExperience.js
@@ -0,0 +1,32 @@
+import React, { Fragment, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addExperience } from '../../actions/profile';
+
+const AddExperience = ({ addExperience, history }) => {
+  const [formData, setFormData] = useState({
+    company: '',
+    title: '',
+    location: '',
+    from: '',
+    to: '',
+    current: false,
+    description: ''
+  });
+
+  const [toDateDisabled, toggleDisabled] = useState(false);
+
+  const { company, title, location, from, to, current, description } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return <Fragment></Fragment>;
+};
+
+AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired
+};
+
+export default connect(null, { addExperience })(AddExperience);
