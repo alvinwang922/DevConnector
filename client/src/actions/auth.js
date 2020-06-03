49db4bbaa4c2abf8a42a60f029910bf2278d6e03
Alvin Wang <alvinwang922@gmail.com>
2020-06-03 14:02:26 -0700

    Fixed infinite loading bug

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -98,6 +98,5 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear Profile
 export const logout = () => dispatch => {
-  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
